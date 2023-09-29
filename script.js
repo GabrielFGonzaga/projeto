71309051ddfbe572b9e79a43f8715f597804a6d7
Gabriel Gonzaga <gabrielfigueiredo05@hotmail.com>
2023-09-29 19:34:53 -0300

    Profile hub

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ function toggleMode () {
   }
 }
 
-function nameclick() {
-  
+function toggleMenu() {
+  const menuMobile = document.getElementById("menu-mobile")
+
+  if(menuMobile.className === "menu-mobile-active"){
+    menuMobile.className = "menu-mobile"
+  }
+  else {
+    menuMobile.className = "menu-mobile-active"
+  }
 }
